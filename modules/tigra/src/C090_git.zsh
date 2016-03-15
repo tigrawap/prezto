95cb71089c307e934b58b2ce9db9b9454bb92337
Anton Bykov <tigrawap@gmail.com>
2016-03-15 14:22:34 +0200

    GCD: Better error handling

--- modules/tigra/src/C090_git.zsh
+++ modules/tigra/src/C090_git.zsh
@@ -11,8 +11,12 @@ function gcd(){
             reponame=`git remote -v | grep origin | head -n1 | awk '{print $2}' | sed -n 's/.*\/\(.*\).git/\1/p'`
             noslashname=`echo $@ | sed -n 's/\//-/p'`
             gcd_target_dir="../$reponame-$noslashname"
-            git worktree add $gcd_target_dir $@ || echo "Failed to create worktree automatically" && return 1
-            cd "$gcd_target_dir"
+            if git worktree add $gcd_target_dir $@ ; then
+                cd "$gcd_target_dir"
+            else
+                echo "Failed to create worktree automatically"
+                return 1
+            fi
         fi
     fi
 }
