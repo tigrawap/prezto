12dc63c11c0f0ef763c9029c041adbca45700d43
Anton Bykov <tigrawap@gmail.com>
2018-12-13 13:13:57 +0200

    Some more aliases

--- modules/tigra/src/C070_aliases.zsh
+++ modules/tigra/src/C070_aliases.zsh
@@ -4,6 +4,7 @@ alias setvimmode="set -o vi"
 alias py=ipython
 alias gpr="git pull --rebase"
 alias gl='git log --date=relative --decorate --format=format:"%an%x09 : %C(yellow)%h%Creset %C(yellow reverse)%d%Creset: %s %C(bold green)(%ar)%C(reset) " --graph'
+alias glr="git log --date=relative --decorate --format=format:'%an%x09 : %C(yellow)%h%Creset %C(yellow reverse)%d%Creset: %s %C(bold green)(%ar)%C(reset) ' --graph | head -n 500 | slit"
 alias sshu='ssh -l ubuntu'
 alias l='ls -lahrt'
 alias ccat='pygmentize -g'
